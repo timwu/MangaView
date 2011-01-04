0b6f4a69969bc3283b4fa229c433076161c95a2d
Tim Wu <tim.wu.0@gmail.com>
2011-01-03 23:05:33 -0800

    Enable page turning and multitouch controller. Totally buggy...

--- src/com/timwu/MangaView/MangaPageView.java
+++ src/com/timwu/MangaView/MangaPageView.java
@@ -1,5 +1,10 @@
 package com.timwu.MangaView;
 
+import org.metalev.multitouch.controller.MultiTouchController;
+import org.metalev.multitouch.controller.MultiTouchController.MultiTouchObjectCanvas;
+import org.metalev.multitouch.controller.MultiTouchController.PointInfo;
+import org.metalev.multitouch.controller.MultiTouchController.PositionAndScale;
+
 import android.content.Context;
 import android.graphics.Matrix;
 import android.util.AttributeSet;
@@ -7,13 +12,15 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 
-public class MangaPageView extends ImageView {
+public class MangaPageView extends ImageView implements MultiTouchObjectCanvas<MangaPageView> {
 	private static final String TAG = MangaPageView.class.getSimpleName();
+	private static final float PAGE_TURN_THRESHOLD_RATIO = 0.125f;
 	
-	private Matrix scaleMatrix = new Matrix();
+	private IMangaController controller;
+	private MultiTouchController<MangaPageView> multiTouchController = new MultiTouchController<MangaPageView>(this);
 	private Matrix currentMatrix = new Matrix();
-	private float startY, ty, ay;
-	private float imageHeight, imageWidth;
+	private float scale, minScale; //minScale clamps the scaling to fit the width of the screen at least.
+	private float xOff, yOff, pageTurnThreshold;
 	
 	public MangaPageView(Context context) {
 		super(context);
@@ -30,44 +37,66 @@ public class MangaPageView extends ImageView {
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
-		float scaleFactor = w;
-		scaleFactor /= getDrawable().getIntrinsicWidth();
-		imageHeight = scaleFactor * getDrawable().getIntrinsicHeight();
-		imageWidth = w;
-		scaleMatrix.reset();
-		scaleMatrix.setScale(scaleFactor, scaleFactor);
-		setImageMatrix(scaleMatrix);
-		ty = 0;
-		ay = 0;
-		Log.d(TAG, "View heigh " + h);
+		resetImageScale();
+	}
+	
+	private void resetImageScale() {
+		xOff = 0;
+		yOff = 0;
+		minScale = scale = (getWidth() * 1.0f) / getDrawable().getIntrinsicWidth();
+		pageTurnThreshold = PAGE_TURN_THRESHOLD_RATIO * getDrawable().getIntrinsicWidth();
+		currentMatrix.setScale(scale, scale);
+		setImageMatrix(currentMatrix);
 	}
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		super.onTouchEvent(event);
-		switch(event.getActionMasked()) {
-		case MotionEvent.ACTION_DOWN:
-			ay = ty;
-			startY = event.getY();
-			Log.d(TAG, "Got some touch down at (" + event.getX() + ", " + event.getY() + ").");
-			break;
-		case MotionEvent.ACTION_MOVE:
-			currentMatrix.set(scaleMatrix);
-			float dy = event.getY() - startY;
-			if (ay + dy > 0) {
-				ty = 0; //at the top
-			} else if (ay + dy < getHeight() - imageHeight) {
-				ty = getHeight() - imageHeight; //at the bottom
-			} else {
-				ty = ay + dy;
-			}
-			currentMatrix.postTranslate(0, ty);
-			Log.d(TAG, "Got a move event, delta y " + dy);
-			Log.d(TAG, "Total translation " + ty);
-			break;
-		}
-		Log.d(TAG, "Bounding rect for drawable " + getDrawable().getBounds().toShortString());
+		return multiTouchController.onTouchEvent(event);
+	}
+	
+	public void setMangaController(IMangaController controller) {
+		this.controller = controller;
+	}
+	
+	public interface IMangaController {
+		public void nextPage();
+		public void prevPage();
+	}
+
+	@Override
+	public MangaPageView getDraggableObjectAtPoint(PointInfo touchPoint) {
+		return this;
+	}
+
+	@Override
+	public void getPositionAndScale(MangaPageView obj,
+			PositionAndScale objPosAndScaleOut) {
+		objPosAndScaleOut.set(xOff, yOff, true, scale, false, 0.0f, 0.0f, false, 0.0f);
+	}
+
+	@Override
+	public boolean setPositionAndScale(MangaPageView obj,
+			PositionAndScale newObjPosAndScale, PointInfo touchPoint) {
+		yOff = newObjPosAndScale.getYOff();
+		xOff = newObjPosAndScale.getXOff();
+		scale = newObjPosAndScale.getScale() >= minScale ? newObjPosAndScale.getScale() : scale ;
+		currentMatrix.reset();
+		currentMatrix.postScale(scale, scale);
+		currentMatrix.postTranslate(0, yOff);
 		setImageMatrix(currentMatrix);
+		Log.i(TAG, "xOff " + xOff + " threshold " + pageTurnThreshold);
+		if (xOff > pageTurnThreshold) {
+			controller.prevPage();
+			resetImageScale();
+		} else if (xOff < -pageTurnThreshold) {
+			controller.nextPage();
+			resetImageScale();
+		}
 		return true;
+	}
+
+	@Override
+	public void selectObject(MangaPageView obj, PointInfo touchPoint) {
+		// Ignore
 	}
 }
--- src/com/timwu/MangaView/Read.java
+++ src/com/timwu/MangaView/Read.java
@@ -1,17 +1,21 @@
 package com.timwu.MangaView;
 
+import com.timwu.MangaView.MangaPageView.IMangaController;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class Read extends Activity {
+public class Read extends Activity implements IMangaController {
 	
 	private MangaVolume vol;
+	private int currentPage = -1;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.read);
+		getMangaPageView().setMangaController(this);
 	}
 
 	@Override
@@ -20,7 +24,19 @@ public class Read extends Activity {
 		if (Intent.ACTION_VIEW.equals(getIntent().getAction())) {
 			vol = MangaVolume.getMangaVolumeForUri(getIntent().getData());
 		}
-		getMangaPageView().setImageDrawable(vol.getPage(0));
+		nextPage();
+	}
+	
+	@Override
+	public void nextPage() {
+		currentPage = (currentPage + 1) % vol.getNumberOfPages();
+		getMangaPageView().setImageDrawable(vol.getPage(currentPage));
+	}
+	
+	@Override
+	public void prevPage() {
+		currentPage = currentPage == 0 ? vol.getNumberOfPages() : currentPage - 1;
+		getMangaPageView().setImageDrawable(vol.getPage(currentPage));
 	}
 	
 	private MangaPageView getMangaPageView() {
